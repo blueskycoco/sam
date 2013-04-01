4fed63749c6511ebd046e42ec6bc3cdaf7f0cc46
bluesky.coco <bluesky.coco@gmail.com>
2013-04-01 09:25:06 +0800

    add envsetup.sh

---
+++ envsetup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+export ARCH=arm
+export CROSS_COMPILE=/home/rdp/work/samsung/s3c6410/arm-2009q3/bin/arm-linux-
+export INITRAMFS_SOURCE_PATH=/home/rdp/work/samsung/s3c6410/tom3q/initramfs-3.0
